6849fe2b39a1d5e4229311c4ddf10944ed5a5473
jingjing20 <1269928993@qq.com>
2020-03-03 14:57:27 +0800

    修改取首页轮播图的方式

--- cloudmusic/miniprogram/pages/playlist/playlist.js
+++ cloudmusic/miniprogram/pages/playlist/playlist.js
@@ -1,4 +1,5 @@
 // pages/music/music.js
+const db = wx.cloud.database()
 const MAX_LIMIT = 15
 Page({
 
@@ -7,15 +8,15 @@ Page({
    */
   data: {
     swiperImgUrls:[
-      {
-        url: 'http://p1.music.126.net/oeH9rlBAj3UNkhOmfog8Hw==/109951164169407335.jpg',
-      },
-      {
-        url: 'http://p1.music.126.net/xhWAaHI-SIYP8ZMzL9NOqg==/109951164167032995.jpg',
-      },
-      {
-        url: 'http://p1.music.126.net/Yo-FjrJTQ9clkDkuUCTtUg==/109951164169441928.jpg',
-      }
+      // {
+      //   url: 'http://p1.music.126.net/oeH9rlBAj3UNkhOmfog8Hw==/109951164169407335.jpg',
+      // },
+      // {
+      //   url: 'http://p1.music.126.net/xhWAaHI-SIYP8ZMzL9NOqg==/109951164167032995.jpg',
+      // },
+      // {
+      //   url: 'http://p1.music.126.net/Yo-FjrJTQ9clkDkuUCTtUg==/109951164169441928.jpg',
+      // }
     ],
     playlist:[]
   },
@@ -25,6 +26,7 @@ Page({
    */
   onLoad: function (options) {
     this._getplaylist();
+    this._getSwiper()
   },
 
   /**
@@ -63,6 +65,7 @@ Page({
       playlist: []
     })
     this._getplaylist()
+    this._getSwiper()
   },
 
   /**
@@ -96,6 +99,13 @@ Page({
       })
       wx.stopPullDownRefresh()
       wx.hideLoading()
+    })
+  },
+  _getSwiper() {
+    db.collection('swiper').get().then((res) => {
+      this.setData({
+        swiperImgUrls: res.data
+      })
     })
   }
 })
